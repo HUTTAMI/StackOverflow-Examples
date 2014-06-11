fee5f3307ce18c1f78d8e2973d89c4e4343a74a9
Brad Christie <brad.christie@gmail.com>
2014-06-11 11:11:49 -0400

    Added solution and files

---
+++ MvcApplication/Models/Home/IndexVM.cs
@@ -0,0 +1,10 @@
+﻿using MvcApplication.Models.Shared;
+using PagedList;
+
+namespace MvcApplication.Models.Home
+{
+    public class IndexVM
+    {
+        public IPagedList<ProjectVM> Projects { get; set; }
+    }
+}
