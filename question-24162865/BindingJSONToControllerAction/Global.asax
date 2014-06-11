bbb21a525c0fe0a92c54da35e54eea854693e0cc
Brad Christie <brad.christie@gmail.com>
2014-06-11 10:28:16 -0400

    How to send data to controller using ajax/json
    http://stackoverflow.com/q/24162865/298053

---
+++ question-24162865/BindingJSONToControllerAction/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BindingJSONToControllerAction.MvcApplication" Language="C#" %>
---
+++ question-24162865/BindingJSONToControllerAction/Models/MyModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BindingJSONToControllerAction.Models
+{
+    public class MyModel
+    {
+        public Int32? ID { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public String Name { get; set; }
+        public String Status { get; set; }
+    }
+}
