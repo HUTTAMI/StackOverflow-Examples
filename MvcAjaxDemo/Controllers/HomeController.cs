25b2ddaccf1a4c5149b346de7773c5b0b253c80f
Brad Christie <brad.christie@gmail.com>
2014-06-19 15:12:23 -0400

    Added sample project

---
+++ MvcAjaxDemo/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace MvcAjaxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public PartialViewResult Ajax()
+        {
+            return PartialView();
+        }
+    }
+}
