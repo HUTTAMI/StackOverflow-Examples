6dcb1cdfd0b475908d56595252e20992d8761424
Brad Christie <brad.christie@gmail.com>
2014-08-21 14:27:02 -0400

    Added version-specific implementations

---
+++ MyCompany.Sitecore/Util.SC65.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Implementations for ONLY Sitecore v6.2
+ */
+
+namespace MyCompany
+{
+    public partial class Util
+    {
+    }
+}
